9b24e2ab931aec1f6125f2209dd860685fb00357
Flow791 <Flow791@outlook.fr>
2017-09-28 16:30:25 +0200

    Création de l'entité Station

---
+++ BeApp_Velib/Model/StationManager.swift
@@ -0,0 +1,16 @@
+//
+//  StationManager.swift
+//  BeApp_Velib
+//
+//  Created by Florian Baudin on 28/09/2017.
+//  Copyright © 2017 Florian Baudin. All rights reserved.
+//
+
+import Foundation
+
+class StationManager {
+    
+    func loadStation(){
+        
+    }
+}
